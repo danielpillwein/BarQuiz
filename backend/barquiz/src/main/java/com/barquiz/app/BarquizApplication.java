c4a4355b2136deb5d353a8f43fdc0747df364255
Daniel <9132@htl.rennweg.at>
2024-07-17 18:08:10 +0200

    project setup

---
+++ backend/barquiz/src/main/java/com/barquiz/app/BarquizApplication.java
@@ -0,0 +1,13 @@
+package com.barquiz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarquizApplication.class, args);
+	}
+
+}
---
+++ backend/barquiz/src/test/java/com/barquiz/app/BarquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barquiz.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
